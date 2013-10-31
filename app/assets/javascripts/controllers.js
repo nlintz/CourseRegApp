e6305a7b3f8b3216e355cce6665f6e04b71da9a2
Nathan Lintz <nlintz@Nathans-MacBook-Pro.local>
2013-10-31 17:51:40 -0400

    started requirements html

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -3,4 +3,10 @@ var controllers = angular.module('Controllers', []);
 controllers.controller('RequirementsController', ['$scope', function($scope){
 	var userStub = {name:"Sharon Grimshaw", major:"Design", yog:"2015"};
 	$scope.user = userStub;
+
+	var majorReqsStub = [{className:"HFID", available:true},{className:"RPRM", available:true},{className:"ADE", available:false}];
+	var genReqsStub = [{className:"Probstat", available:true},{className:"Design Depth", available:true},{className:"FBE", available:false}];
+
+	$scope.majorReqs = majorReqsStub;
+	$scope.genReqs = genReqsStub;
 }]);
